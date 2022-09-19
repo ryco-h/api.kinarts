e4d7a6856c9c3cd8a25a68d3d388592b0cf9017d
Dudu Subagjo <roaddevil53@gmail.com>
2022-09-20 00:05:17 +0700

    Payment and stuff

--- routes/artCollections.js
+++ routes/artCollections.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const Grid = require('gridfs-stream');
 const { GridFsStorage } = require('multer-gridfs-storage');
 const mongoose = require('mongoose');
+const axios = require('axios')
 
 require('dotenv/config')
 const DB_URL = process.env.DB_URL
@@ -293,5 +294,55 @@ route.delete(`/:id`, async (req, res) => {
    //    }
    // });
 })
+
+ 
+route.get('/donate-artcollection', (req, res) => {
+
+   axios({
+      // Below is the API URL endpoint
+      url: "https://app.midtrans.com/snap/v1/transactions",
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization:
+          "Basic " +
+          Buffer.from("Mid-server-FUV4yJVjTkwNh2664t1ILZ61").toString("base64")
+        // Above is API server key for the Midtrans account, encoded to base64
+      },
+      data:
+        // Below is the HTTP request body in JSON
+        {
+          transaction_details: {
+            order_id: "order-csb-" + req.query.artName.replace(/ /g, '-') + '-' + getCurrentTimestamp(),
+            gross_amount: new Number(req.query.nominal_donasi)
+          },
+          credit_card: {
+            secure: true
+          },
+          customer_details: {
+            first_name: "Guest",
+            last_name: "User",
+            email: "guest_user@email.com",
+            phone: "08111222333"
+          }
+        }
+    }).then(
+      (snapResponse) => {
+        let snapToken = snapResponse.data.token;
+        console.log("Retrieved snap token:", snapToken);
+        // Pass the Snap Token to frontend, render the HTML page
+        res.send(snapToken);
+      },
+      (error) => {
+        res.send(`Fail to call API w/ error ${error}`);
+      //   console.log(error);
+      }
+    );
+});
+
+let getCurrentTimestamp = () => {
+   return "" + Math.round(new Date().getTime() / 1000);
+ }; 
 
 module.exports = route
